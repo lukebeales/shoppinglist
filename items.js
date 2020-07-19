eccd78fec23e202447c0d2001c145096a1d14b69
root <root@lukebeales.com>
2020-07-19 15:36:42 +1000

    initial commit

---
+++ items.js
@@ -0,0 +1,107 @@
+itemAdd('Woolies', 'Coffee Luke');
+itemAdd('Woolies', 'Coffee Anita');
+itemAdd('Woolies', 'Coffee Jar');
+itemAdd('Woolies', 'Milk Luke');
+itemAdd('Woolies', 'Milk Anita');
+itemAdd('Woolies', 'Almond Milk');
+
+itemAdd('Woolies', 'Tomato Relish');
+itemAdd('Woolies', 'Dried Apricots');
+itemAdd('Woolies', 'Dry Biscuits');
+itemAdd('Woolies', 'Cashews / Nuts');
+itemAdd('Woolies', 'Cranberries');
+itemAdd('Woolies', 'Pasta');
+itemAdd('Woolies', 'Rice');
+itemAdd('Woolies', 'Honey');
+itemAdd('Woolies', 'Corn Chips');
+itemAdd('Woolies', 'Sugar Raw');
+
+itemAdd('Woolies', 'Freezer Bags');
+itemAdd('Woolies', 'Baking paper');
+itemAdd('Woolies', 'Glad wrap');
+itemAdd('Woolies', 'Cuppa Soup Chick / Mushroom');
+itemAdd('Woolies', 'Balsamic Vinegar');
+itemAdd('Woolies', 'Gerkin Bottled');
+itemAdd('Woolies', 'Pickled Onion');
+
+itemAdd('Woolies', 'Bread');
+itemAdd('Woolies', 'Wraps');
+itemAdd('Woolies', 'Bread Rolls');
+
+itemAdd('Woolies', 'Dish washer powder');
+
+itemAdd('Woolies', 'Deodorant');
+itemAdd('Woolies', 'Toothpaste');
+itemAdd('Woolies', 'Shampoo');
+itemAdd('Woolies', 'Conditioner');
+itemAdd('Woolies', 'Ear Qtips');
+
+itemAdd('Aldi', 'Cheese');
+itemAdd('Aldi', 'Laundry Detergent');
+itemAdd('Aldi', 'Rubbish Bags');
+itemAdd('Aldi', 'Paper Towels');
+itemAdd('Aldi', 'Tissues');
+itemAdd('Aldi', 'Chocolate');
+itemAdd('Aldi', 'Coconut Milk');
+
+itemAdd('Sauces', 'Spicy Red');
+itemAdd('Sauces', 'Holbrooks');
+itemAdd('Sauces', 'Tomato');
+itemAdd('Sauces', 'BBQ');
+itemAdd('Sauces', 'HP');
+itemAdd('Sauces', 'Greek Lemon Dressing');
+itemAdd('Sauces', 'Caesar Salad Dressing');
+
+itemAdd('Fruit & Veges', 'Avocado');
+itemAdd('Fruit & Veges', 'Tomatos / Cherry');
+itemAdd('Fruit & Veges', 'Lettuce');
+itemAdd('Fruit & Veges', 'Potatos');
+itemAdd('Fruit & Veges', 'Carrots');
+itemAdd('Fruit & Veges', 'Sweet Potato');
+itemAdd('Fruit & Veges', 'Mushrooms');
+itemAdd('Fruit & Veges', 'Celery');
+itemAdd('Fruit & Veges', 'Spring Onions');
+itemAdd('Fruit & Veges', 'Spinach');
+itemAdd('Fruit & Veges', 'Snow Peas');
+itemAdd('Fruit & Veges', 'Zucchini');
+itemAdd('Fruit & Veges', 'Garlic');
+itemAdd('Fruit & Veges', 'Mandarins');
+itemAdd('Fruit & Veges', 'Oranges');
+itemAdd('Fruit & Veges', 'Bananas');
+itemAdd('Fruit & Veges', 'Pineapple');
+itemAdd('Fruit & Veges', 'Watermelon');
+itemAdd('Fruit & Veges', 'Apples');
+itemAdd('Fruit & Veges', 'Pumpkin');
+itemAdd('Fruit & Veges', 'Rockmelon');
+itemAdd('Fruit & Veges', 'Onions');
+
+itemAdd('Meat & Fish', 'Chicken');
+itemAdd('Meat & Fish', 'Casserole Steak');
+itemAdd('Meat & Fish', 'Sausages');
+itemAdd('Meat & Fish', 'Stir Fry Steak');
+itemAdd('Meat & Fish', 'Mince');
+itemAdd('Meat & Fish', 'Chicken Mince');
+itemAdd('Meat & Fish', 'Pork Mince');
+itemAdd('Meat & Fish', 'Fish / Salmon');
+itemAdd('Meat & Fish', 'Chicken Necks');
+itemAdd('Meat & Fish', 'Doggie Mince');
+itemAdd('Meat & Fish', 'Pork steak');
+
+itemAdd('Cold Things', 'Yoghurt');
+itemAdd('Cold Things', 'Greek Yogurt');
+itemAdd('Cold Things', 'Eggs');
+itemAdd('Cold Things', 'Sour Cream');
+itemAdd('Cold Things', 'Ham / Cold Meat');
+itemAdd('Cold Things', 'Dip');
+itemAdd('Cold Things', 'Cream');
+
+itemAdd('Freezer', 'Pastry');
+itemAdd('Freezer', 'Peas');
+itemAdd('Freezer', 'Chips');
+itemAdd('Freezer', 'Icecream');
+
+itemAdd('Pet Shop', 'Greenies');
+itemAdd('Pet Shop', 'Treats');
+itemAdd('Pet Shop', 'Cigars');
+itemAdd('Pet Shop', 'Biscuits');
+itemAdd('Pet Shop', 'Food Tins');
